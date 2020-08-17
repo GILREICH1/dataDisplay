36fd9662426e0a6925b2d1f8db17558b25df3d22
Shahar Taite <shahar.taite@perimeterx.com>
2020-08-17 08:29:09 +0300

    fix proxy

--- src/server/index.js
+++ src/server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('dist'));
 app.get('/api/getSomething', (req, res) => res.send({ sounds: 'good' }));
+app.use(express.static('dist'));
 
 app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
