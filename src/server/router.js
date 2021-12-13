b1af4d0eac381d25b18030374b049ca9f7f5bcaa
gilreich1 <gilreich12@gmail.com>
2021-12-13 14:11:42 +0200

    server middleware

--- src/server/router.js
+++ src/server/router.js
@@ -1,9 +1,8 @@
-const { append } = require("express/lib/response");
 const getEvents = require("./controller");
 
 const router = require("express").Router();
 
-router.get("/api/getSomething", (req, res) => res.send({ sounds: "good" }));
+router.get("/api/getSomething", (_, res) => res.send({ sounds: "good" }));
 router.get("/events", getEvents);
 
 module.exports = router;
